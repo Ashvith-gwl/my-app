5e571dfb0201a187749abd87aeeba813939ef103
Ashvith <ashvith@goodworklabs.com>
2019-07-31 18:05:51 +0530

    state

--- src/index.js
+++ src/index.js
@@ -1,10 +1,32 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+function Message(props){
+    if(props.value){
+        return <h1>This is first message</h1>
+    }
+    return <h4>this is second</h4>
+}
 
-setInterval(function(){
+class Btn extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {value:true}
+    }
+    handleClick = () => {
+       this.setState({
+           value : !this.state.value
+       });
+    }
+    render(){
+        return <div>
+        <button onClick={this.handleClick}>Change message</button>
+        <Message value={this.state.value}/>
+        </div>
+    }
+}
+
 ReactDOM.render(
-    <h2>{new Date().toLocaleTimeString()}</h2>,
-   document.getElementById('root') 
+    <Btn />,
+    document.getElementById('root')
 );
-},1000);
